2b9149f638c66923bae1e66941d7db7c3a6ce900
stoswebsolutions <stoswebsolutions@gmail.com>
2023-03-30 22:05:30 +0530

    dashboard page designed

--- backend/app/Controllers/Dashboard.php
+++ backend/app/Controllers/Dashboard.php
@@ -4,12 +4,22 @@ namespace App\Controllers;
 
 class Dashboard extends BaseController
 {
+    private $loggedInfo;
+    public function __construct()
+    {
+        $this->loggedInfo = session()->get('LoggedData');
+    }
+
     public function index()
     {
         $data = [
             'pageTitle' => 'Crusher Administrator | Dashboard',
+            'pageHeading' => 'Dashboard',
+            'loggedInfo' => $this->loggedInfo,
             'logo' => site_url() . 'assets/images/logo.png'
         ];
-        return view('dashboard/index', $data);
+        return view('common/top', $data)
+            . view('dashboard/index')
+            . view('common/bottom');
     }
 }
--- backend/app/Views/dashboard/index.php
+++ backend/app/Views/dashboard/index.php
@@ -1,1 +1,25 @@
-<h1>Welcome Dashboard</h1>
+<div class="preloader">
+    <div class="lds-ripple">
+        <div class="lds-pos"></div>
+        <div class="lds-pos"></div>
+    </div>
+</div>
+<div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full" data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+    <?= view('common/header') ?>
+    <?= view('common/aside') ?>
+    <div class="page-wrapper">
+        <div class="page-breadcrumb">
+            <div class="row">
+                <div class="col-12 d-flex no-block align-items-center">
+                    <h4 class="page-title"><?= $pageHeading ?></h4>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid text-center">
+            <h1>Welcome <?= $loggedInfo['name'] ?><br>
+                Crusher Administrator Telangana,</h1><br>
+            <img src="<?= $logo ?>" class="img-fluid d-flext block">
+        </div>
+    </div>
+    <?= view('common/footer') ?>
+</div>
