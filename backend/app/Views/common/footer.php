6a3a9d743eb8bb7b62ec016f761abf6dee3e468a
stoswebsolutions <stoswebsolutions@gmail.com>
2023-03-30 22:05:06 +0530

    dashboard common files header footer aside

---
+++ backend/app/Views/common/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer text-center">
+    All Rights Reserved by Crusher Management. Designed and Developed by
+    <a href="https://vitasoft.in/">VITA TECH SOLUTIONS PVT LTD</a>.
+</footer>
