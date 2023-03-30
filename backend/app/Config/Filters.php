fd73c544b749c04686c70326ac9bd22559e758f7
stoswebsolutions <stoswebsolutions@gmail.com>
2023-03-30 17:25:58 +0530

    login filters created

--- backend/app/Config/Filters.php
+++ backend/app/Config/Filters.php
@@ -2,6 +2,8 @@
 
 namespace Config;
 
+use App\Filters\AlreadyLoggedInFilter;
+use App\Filters\AuthCheckFilter;
 use CodeIgniter\Config\BaseConfig;
 use CodeIgniter\Filters\CSRF;
 use CodeIgniter\Filters\DebugToolbar;
@@ -21,6 +23,8 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'AuthCheck'     => AuthCheckFilter::class,
+        'AlreadyLoggedIn'         => AlreadyLoggedInFilter::class
     ];
 
     /**
