644de83b6c6becb9c753b5de09e15fa9fb8b1e0f
stoswebsolutions <stoswebsolutions@gmail.com>
2023-04-11 16:08:30 +0530

    dashboard design changed

--- backend/app/Controllers/Dashboard.php
+++ backend/app/Controllers/Dashboard.php
@@ -2,21 +2,94 @@
 
 namespace App\Controllers;
 
+use App\Models\BookingModel;
+use App\Models\ExpensesModel;
+use App\Models\LoginModel;
+use App\Models\PaymentsModel;
+
 class Dashboard extends BaseController
 {
     private $loggedInfo;
+    private $loginModel;
+    private $bookingModel;
+    private $paymentsModel;
+    private $expensesModel;
     public function __construct()
     {
         $this->loggedInfo = session()->get('LoggedData');
+        $this->loginModel = new LoginModel();
+        $this->bookingModel = new BookingModel();
+        $this->paymentsModel = new PaymentsModel();
+        $this->expensesModel = new ExpensesModel();
     }
 
     public function index()
     {
+        $today_loads = $this->bookingModel->where("DATE_FORMAT(load_date,'%Y-%m-%d')", date('Y-m-d'))->select('count(load_id) as today_loads')->first();
+        $today_income = $this->paymentsModel->where("DATE_FORMAT(create_date,'%Y-%m-%d')", date('Y-m-d'))->select('sum(payment_today) as today_income')->first();
+        $today_expenses = $this->expensesModel->where("DATE_FORMAT(payment_date,'%Y-%m-%d')", date('Y-m-d'))->select('sum(amount) as today_expenses')->first();
+        $pending1 = $this->bookingModel->where("DATE_FORMAT(load_date,'%Y-%m-%d')", date('Y-m-d'))->findAll();
+        $today_pending = 0;
+        if (!empty($pending1)) {
+            foreach ($pending1 as $row) {
+                $paymentsInfo = $this->paymentsModel->where('load_id', $row['load_id'])->orderBy('payment_id', 'DESC')->get()->getResult();
+                $due_amount = $paymentsInfo[0]['due_amount'];
+                $today_pending = $today_pending + $due_amount;
+            }
+        }
+        $today = array(
+            'today_loads' => $today_loads['today_loads'] > 0 ? $today_loads['today_loads'] : 0,
+            'today_income' => $today_income['today_income'] > 0 ? $today_income['today_income'] : 0,
+            'today_expenses' => $today_expenses['today_expenses'] > 0 ? $today_expenses['today_expenses'] : 0,
+            'today_pending' => $today_pending > 0 ? $today_pending : 0
+        );
+        $cm_loads = $this->bookingModel->where("DATE_FORMAT(load_date,'%Y-%m')", date('Y-m'))->select('count(load_id) as cm_loads')->first();
+        $cm_income = $this->paymentsModel->where("DATE_FORMAT(create_date,'%Y-%m')", date('Y-m'))->select('sum(payment_today) as cm_income')->first();
+        $cm_expenses = $this->expensesModel->where("DATE_FORMAT(payment_date,'%Y-%m')", date('Y-m'))->select('sum(amount) as cm_expenses')->first();
+        $pending2 = $this->bookingModel->where("DATE_FORMAT(load_date,'%Y-%m')", date('Y-m'))->findAll();
+        $cm_pending = 0;
+        if (!empty($pending2)) {
+            foreach ($pending2 as $row) {
+                $paymentsInfo = $this->paymentsModel->where('load_id', $row['load_id'])->orderBy('payment_id', 'DESC')->findAll();
+                $due_amount = $paymentsInfo[0]['due_amount'];
+                $cm_pending = $cm_pending + $due_amount;
+            }
+        }
+        $cm = array(
+            'cm_loads' => $cm_loads['cm_loads'] > 0 ? $cm_loads['cm_loads'] : 0,
+            'cm_income' => $cm_income['cm_income'] > 0 ? $cm_income['cm_income'] : 0,
+            'cm_expenses' => $cm_expenses['cm_expenses'] > 0 ? $cm_expenses['cm_expenses'] : 0,
+            'cm_pending' => $cm_pending > 0 ? $cm_pending : 0
+        );
+        $cy_loads = $this->bookingModel->where("DATE_FORMAT(load_date,'%Y')", date('Y'))->select('count(load_id) as cy_loads')->first();
+        $cy_income = $this->paymentsModel->where("DATE_FORMAT(create_date,'%Y')", date('Y'))->select('sum(payment_today) as cy_income')->first();
+        $cy_expenses = $this->expensesModel->where("DATE_FORMAT(payment_date,'%Y')", date('Y'))->select('sum(amount) as cy_expenses')->first();
+        $pending3 = $this->bookingModel->where("DATE_FORMAT(load_date,'%Y')", date('Y'))->findAll();
+        $cy_pending = 0;
+        if (!empty($pending3)) {
+            foreach ($pending3 as $row) {
+                $paymentsInfo = $this->paymentsModel->where('load_id', $row['load_id'])->orderBy('payment_id', 'DESC')->findAll();
+                $due_amount = $paymentsInfo[0]['due_amount'];
+                $cy_pending = $cy_pending + $due_amount;
+            }
+        }
+        $cy = array(
+            'cy_loads' => $cy_loads['cy_loads'] > 0 ? $cy_loads['cy_loads'] : 0,
+            'cy_income' => $cy_income['cy_income'] > 0 ? $cy_income['cy_income'] : 0,
+            'cy_expenses' => $cy_expenses['cy_expenses'] > 0 ? $cy_expenses['cy_expenses'] : 0,
+            'cy_pending' => $cy_pending > 0 ? $cy_pending : 0
+        );
+        $customers = $this->loginModel->where("login_id <>", 2)->select('count(login_id) as customers')->first();
+
         $data = [
             'pageTitle' => 'Crusher Administrator | Dashboard',
             'pageHeading' => 'Dashboard',
             'loggedInfo' => $this->loggedInfo,
-            'logo' => site_url() . 'assets/images/logo.png'
+            'logo' => site_url() . 'assets/images/logo.png',
+            'today' => $today,
+            'cm' => $cm,
+            'cy' => $cy,
+            'customers'    => $customers['customers'] > 0 ? $customers['customers'] : '0',
         ];
         return view('common/top', $data)
             . view('dashboard/index')
--- backend/app/Views/dashboard/index.php
+++ backend/app/Views/dashboard/index.php
@@ -15,10 +15,140 @@
                 </div>
             </div>
         </div>
-        <div class="container-fluid text-center">
-            <h1>Welcome <?= $loggedInfo['name'] ?><br>
-                Crusher Administrator Telangana,</h1><br>
-            <img src="<?= $logo ?>" class="img-fluid d-flext block">
+        <div class="container-fluid">
+            <h1>Welcome <?= $loggedInfo['name'] ?>,
+                <div class="row mt-3">
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-cyan text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $today['today_loads'] ?>
+                                </h1>
+                                <h6 class="text-white">Loads Today</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-success text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $today['today_income'] ?>
+                                </h1>
+                                <h6 class="text-white">Income Today</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-warning text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $today['today_expenses'] ?>
+                                </h1>
+                                <h6 class="text-white">Expenses Today</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-danger text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $today['today_pending'] ?>
+                                </h1>
+                                <h6 class="text-white">Pending Today</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-info text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $cm['cm_loads'] ?>
+                                </h1>
+                                <h6 class="text-white">Loads Month</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-danger text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $cm['cm_income'] ?>
+                                </h1>
+                                <h6 class="text-white">Income Month</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-info text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $cm['cm_expenses'] ?>
+                                </h1>
+                                <h6 class="text-white">Expenses Month</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-cyan text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $cm['cm_pending'] ?>
+                                </h1>
+                                <h6 class="text-white">Pending Month</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-success text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $cy['cy_loads'] ?>
+                                </h1>
+                                <h6 class="text-white">Loads Year</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-warning text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $cy['cy_income'] ?>
+                                </h1>
+                                <h6 class="text-white">Income Year</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-danger text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $cy['cy_expenses'] ?>
+                                </h1>
+                                <h6 class="text-white">Expenses Year</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-info text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $cy['cy_pending'] ?>
+                                </h1>
+                                <h6 class="text-white">Pending Year</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-danger text-center">
+                                <h1 class="font-light text-white">
+                                    <?= $customers ?>
+                                </h1>
+                                <h6 class="text-white">Customers</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
     <?= view('common/footer') ?>
