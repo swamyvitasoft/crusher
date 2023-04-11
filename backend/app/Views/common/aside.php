f4c0633aa781b0ee7f258174e7aac22c7d9676aa
stoswebsolutions <stoswebsolutions@gmail.com>
2023-04-11 16:07:18 +0530

    some root links removed

--- backend/app/Views/common/aside.php
+++ backend/app/Views/common/aside.php
@@ -10,19 +10,19 @@ use App\Libraries\Hash;
                     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= site_url() ?>dashboard/<?= Hash::path('index') ?>" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</span></a>
                 </li>
                 <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= site_url() ?>pricelist/<?= Hash::path('view') ?>/0" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">PriceList</span></a>
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= site_url() ?>pricelist/<?= Hash::path('view') ?>/0" aria-expanded="false"><i class="mdi mdi-view-list"></i><span class="hide-menu">PriceList</span></a>
                 </li>
                 <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= site_url() ?>booking/<?= Hash::path('view') ?>" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Booking</span></a>
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= site_url() ?>booking/<?= Hash::path('view') ?>" aria-expanded="false"><i class="mdi mdi-cart"></i><span class="hide-menu">Booking</span></a>
                 </li>
                 <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= site_url() ?>expenses/<?= Hash::path('view') ?>" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Expenses</span></a>
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= site_url() ?>expenses/<?= Hash::path('view') ?>" aria-expanded="false"><i class="mdi mdi-cash"></i><span class="hide-menu">Expenses</span></a>
                 </li>
                 <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= site_url() ?>customers/<?= Hash::path('view') ?>" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Customers</span></a>
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= site_url() ?>customers/<?= Hash::path('view') ?>" aria-expanded="false"><i class="mdi mdi-account"></i><span class="hide-menu">Customers</span></a>
                 </li>
                 <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= site_url() ?>reports/<?= Hash::path('index') ?>/all" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Reports</span></a>
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= site_url() ?>reports/<?= Hash::path('index') ?>/all" aria-expanded="false"><i class="mdi mdi-printer"></i><span class="hide-menu">Reports</span></a>
                 </li>
             </ul>
         </nav>
